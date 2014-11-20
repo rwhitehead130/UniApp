be9563f85ebbc922757a7e511fb6caa408877c02
Robert Whitehead <rwhitehead130@gmail.com>
2014-11-21 11:02:08 +1300

    10:54-2014-11-21

--- www/js/index.js
+++ www/js/index.js
@@ -33,7 +33,6 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-		console.log(navigator.camera);
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
